769e8934470326f62531dcd68a6ca6828fd029cc
kshshton <michkrzyszton@gmail.com>
2023-06-20 01:02:46 +0200

    Updated requirements.txt

--- front-end/src/App.svelte
+++ front-end/src/App.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
     import Products from './lib/Products.svelte';
-  import viteLogo from '/vite.svg'
 </script>
 
 <main>
@@ -9,21 +7,3 @@
     <Products />
   </div>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
--- front-end/src/lib/Products.svelte
+++ front-end/src/lib/Products.svelte
@@ -1,18 +1,18 @@
 <script>
-	const fetchProducts = (async () => {
-		const response = await fetch('http://127.0.0.1:8000/all_products/')
+  const fetchProducts = (async () => {
+    const response = await fetch('http://127.0.0.1:8000/all_products/')
     return await response.json()
-	})()
+  })()
 </script>
 
 {#await fetchProducts}
 	<p>...waiting</p>
 {:then product}
-    {#each product as { name, category, quantity }, i }
-        <p>
-            {i + 1}: {name} - {category} - {quantity}
-        </p>
-    {/each}
+  {#each product as { name, category, quantity }, i }
+    <p>
+      {i + 1}: {name} - {category} - {quantity}
+    </p>
+  {/each}
 {:catch error}
 	<p>Error: {error}</p>
 {/await}
